23003c7e7051b3c88f330bdaab78a069b1ef1874
Tom Kamphuis <tom.kamphuis@snappet.org>
2018-04-30 17:08:37 +0200

    Test AF try 1

--- TomKamphuis.Web/ClientApp/components/Home.tsx
+++ TomKamphuis.Web/ClientApp/components/Home.tsx
@@ -7,6 +7,9 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
             <h1>Hello, I am robot!</h1>
             <p>This is a mere example of how to build and deploy an application using .NET Core and React. This is a good question...</p>
 			<p>Test to interact with Azure functions and BetterCodeHub.</p>
+			<ul>
+				<li>Attempt 1</li>
+			</ul>
         </div>;
     }
 }
