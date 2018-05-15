0d04b7c0448071834c867ab702c9a3ec4c17724e
Tom Kamphuis <tom.kamphuis@snappet.org>
2018-05-15 17:35:56 +0200

    Added some extra sample product info

--- TomKamphuis.Repositories/Implementations/ProductRepository.cs
+++ TomKamphuis.Repositories/Implementations/ProductRepository.cs
@@ -9,12 +9,34 @@ namespace TomKamphuis.Repositories.Implementations
 	{
 		private readonly List<Product> _products = new List<Product>
 		{
+
 			new Product
 			{
 				Id = 1,
+				Name = "Snappet",
+				Description = "Our mission is to improve learning outcome of children worldwide. We do this by developping multiple applications that support each other.",
+				WebsiteUrl = "snappet.org"
+			},
+			new Product
+			{
+				Id = 2,
 				Name = "Funda",
 				Description = "Funda is the biggest realtor website in the Netherlands",
 				WebsiteUrl = "funda.nl"
+			},
+			new Product
+			{
+				Id = 3,
+				Name = "Transavia",
+				Description = "Transavia b2c was focussed on creating the new award winning platform to help people book their flight",
+				WebsiteUrl = "transavia.com"
+			},
+			new Product
+			{
+				Id = 4,
+				Name = "DTG",
+				Description = "Setting up the website used in their new b2b strategy",
+				WebsiteUrl = "dtg.nl"
 			}
 		};
 
